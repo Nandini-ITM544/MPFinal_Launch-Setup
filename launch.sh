cd93167f6ac24565985714c3156a539e97f6cb07
Nandini <nandini9590@gmail.com>
2015-11-24 01:08:22 -0600

    adding sticky policy

--- launch.sh
+++ launch.sh
@@ -8,6 +8,8 @@ aws ec2 wait instance-running --instance-ids ${instance_id[@]}
 aws elb create-load-balancer --load-balancer-name Project1 --listeners Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80 --security-groups $5 --subnets $6
 aws elb register-instances-with-load-balancer --load-balancer-name Project1 --instances ${instance_id[@]}
 aws elb configure-health-check --load-balancer-name Project1 --health-check Target=HTTP:80/index.html,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+aws elb create-lb-cookie-stickiness-policy --load-balancer-name Project1 --policy-name StickyPloicy
+aws elb set-load-balancer-policies-of-listener --load-balancer-name Project1 --policy-name StickyPloicy --load-balancer-port 80
 aws autoscaling create-launch-configuration --launch-configuration-name Project1-launch-config --image-id $1 --key-name $4 --security-groups $5 --instance-type $3 --user-data /home/controller/Documents/MPFinal_Environment_Setup/install-webserver.sh --iam-instance-profile $7
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name Project1-auto-scaling-group-2 --launch-configuration-name Project1-launch-config --load-balancer-names Project1 --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $6	
 aws autoscaling put-scaling-policy --auto-scaling-group-name Project1-auto-scaling-group-2 --policy-name scalingpolicytest --scaling-adjustment 1 --adjustment-type ExactCapacity
